24ccb779505963eb38313652768db2fce2c5e637
Gyp the Cat <47790678+gypthecat@users.noreply.github.com>
2024-06-23 22:19:30 +0100

    Comments in sh

--- generatekustomaxmind.sh
+++ generatekustomaxmind.sh
@@ -1,11 +1,16 @@
+# Download from Maxmind
 wget "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-ASN-CSV&license_key=$1&suffix=zip" --output-document=GeoLite2-ASN.zip --no-check-certificate
 
+# Unzip to here
 unzip GeoLite2-ASN.zip -d .
 
+# Move the files to this directory
 mv GeoLite2-ASN-CSV*/*.csv .
 
+# Remove the top rows of the CSVs
 sed -i 1d *.csv
 
+# Create tables to import the data
 echo '
 CREATE TABLE CIDRASNIPV4(
   "CIDR" TEXT,
@@ -24,6 +29,7 @@ CREATE TABLE CIDRASNIPV6(
 .schema
 ' | sqlite3 temp.db
 
+# Import the data
 echo '
 .mode csv
 .import GeoLite2-ASN-Blocks-IPv4.csv CIDRASNIPV4
@@ -50,11 +56,17 @@ echo '
 SELECT CIDR, CIDRASN, CIDRASNName, "GeoLite2 by MaxMind" as CIDRSource FROM CIDRASNIPV6;
 ' | sqlite3 temp.db
 
+# Compress the files
 zip -9 -j kusto-cidr-asn.csv.zip kusto-cidr-asn.csv
 zip -9 -j kusto-cidr-asn-ipv6.csv.zip kusto-cidr-asn-ipv6.csv
 
+# Necessary Python packages
 pip install pandas fastparquet pyarrow
 
+# Generate Parquet files
 python3 generateparquet.py
 
+# Sanity check contents
 ls -lha
+
+# You can do something here with the files, eg upload elsewhere
